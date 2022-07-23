4128401b84b0a79300e9c3882a5ca2ef3d5aefb9
k12re <83732570+k12re@users.noreply.github.com>
2022-07-23 21:46:25 +0200

    checked and corrected against answers

--- js/script.js
+++ js/script.js
@@ -1,95 +1,66 @@
 //Question 1
 
-let heading = document.querySelector("h3");
-
-function changeHeading() {
-  heading.innerHTML = "Hello";
-}
-
-changeHeading();
+const heading = document.querySelector("h3");
+heading.innerHTML = "Hello";
 
 //Question 2
-document.querySelector("h4").style.marginBottom = "-20px";
-//NEED ANSWER
-//let subHeading = document.querySelector("h4"); //querySelectorAll wont work
 
-//function changeMargin() {
-//  subHeading.style.marginBottom = "-20px";
-//}
+const subHeading = document.querySelectorAll("h4");
 
-//changeMargin();
+for (let i = 0; i < subHeading.length; i++) {
+  subHeading[i].style.marginBottom = "-20px";
+}
 
 //Question 3
 
-let subHeadingRed = document.querySelector("h4.two");
-
-function changeColor() {
-  subHeadingRed.style.backgroundColor = "red";
-}
-
-changeColor();
+subHeading[1].style.backgroundColor = "red"; //Uses above loop for ref
+//OR
+//let subHeadingRed = document.querySelector("h4.two");
+//subHeadingRed.style.backgroundColor = "red";
 
 //Question 4
 
-document.querySelector("p").style.fontSize = "24px";
-document.querySelector("p").style.color = "blue";
+//document.querySelector("p.one").style.fontSize = "24px";
+//document.querySelector("p.one").style.color = "blue";
+
+//OR
+
+const firstParagraph = document.querySelector("p.one");
+firstParagraph.style.fontSize = "24px";
+firstParagraph.style.color = "blue";
 
 //Question 5
 
-let changeParagraph = document.querySelector(".section.three");
-
-function addSpan() {
-  changeParagraph.innerHTML = "<span>" + changeParagraph.innerHTML + "</span>";
-}
-
-addSpan();
+const changeParagraph = document.querySelector("p.three");
+changeParagraph.innerHTML = "<span>" + changeParagraph.innerHTML + "</span>";
+//OR
+//changeParagraph.innerHTML = `<span>${changeParagraph.innerHTML}</span>`;
 
 //Question 6
-//NEED ANSWER
-let addParagraph = document.querySelectorAll(".content");
 
-function pushParagraph() {
-  addParagraph.push = "<p>";
-}
-
-pushParagraph();
+const addParagraph = document.querySelector(".content");
+addParagraph.innerHTML += "<p>Another paragraph</p>";
 
 //Question 7
-//NEED ANSWER
-let addClass = document.querySelector(".content");
 
-function contentItem() {
-  addClass.innerHTML = "<.content-item>" + addClass.innerHTML + "</.content-item>";
+const addClass = document.querySelectorAll(".content p");
+
+for (let i = 0; i < addClass.length; i++) {
+  addClass[i].classList.add("content-item");
 }
-
-contentItem();
 
 //Question 8
 
-let listItem = document.querySelector("#services");
-
-function addListItem() {
-  listItem.innerHTML = "<li>" + "Item" + "</li>" + listItem.innerHTML;
-}
-
-addListItem();
+const listItem = document.querySelector("#services");
+listItem.innerHTML = "<li>New item</li>" + listItem.innerHTML;
 
 //Question 9
-//NEED ANSWER
-let serviceList = document.querySelector(".service-list");
 
-function removeClass() {
-  serviceList.innerHTML = "";
-}
-
-removeClass();
+listItem.classList.remove("service-list");
 
 //Question 10
 
 let hideElement = document.querySelector("#hide");
-
-function letsHideElement() {
-  hideElement.hidden = true;
-}
-
-letsHideElement();
+hideElement.hidden = true;
+//OR
+//hideElement.style.display = "none";
